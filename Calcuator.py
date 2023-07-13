7f7e2b97ee4c058b20a65f92791f9efd4b932d19
Code Lover <139486665+Codelover-python@users.noreply.github.com>
2023-07-13 21:57:56 +0530

    Python Program to make a calculator

---
+++ Calcuator.py
@@ -0,0 +1,50 @@
+# Python program for simple calculator
+
+# Function to add two numbers and def function 1
+def add(num1, num2):
+	return num1 + num2
+
+# Function to subtract two numbers and def function 2
+def subtract(num1, num2):
+	return num1 - num2
+
+# Function to multiply two numbers and def function 3
+def multiply(num1, num2):
+	return num1 * num2
+
+# Function to divide two numbers an def function 4
+def divide(num1, num2):
+	return num1 / num2
+
+#the print function
+print("Please select operation -\n" \
+		"1. Add\n" \
+		"2. Subtract\n" \
+		"3. Multiply\n" \
+		"4. Divide\n")
+
+
+# Take input from the user
+select = int(input("Select operations form 1, 2, 3, 4 :"))
+
+number_1 = int(input("Enter first number: "))
+number_2 = int(input("Enter second number: "))
+
+#if else statement
+if select == 1:
+	print(number_1, "+", number_2, "=",
+					add(number_1, number_2))
+
+elif select == 2:
+	print(number_1, "-", number_2, "=",
+					subtract(number_1, number_2))
+
+elif select == 3:
+	print(number_1, "*", number_2, "=",
+					multiply(number_1, number_2))
+
+elif select == 4:
+	print(number_1, "/", number_2, "=",
+					divide(number_1, number_2))
+else:
+	print("Invalid input")
